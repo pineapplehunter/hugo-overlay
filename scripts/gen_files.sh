5a1807f32bcda2a0b5e1bebbb3d018db18d3224e
Shogo Takata <peshogo@gmail.com>
2025-07-26 12:00:38 +0900

    retry curl script

--- scripts/gen_files.sh
+++ scripts/gen_files.sh
@@ -6,7 +6,7 @@ set -exuo pipefail
 SCRIPT_DIR=$(dirname "$0")
 
 tags(){
-  curl -L --proto '=https' --tlsv1.2 -sSf "https://api.github.com/repos/gohugoio/hugo/git/refs/tags" | \
+  curl -L --retry 5 "https://api.github.com/repos/gohugoio/hugo/git/refs/tags" | \
     jq -r ".[].ref" | \
     sed "s|refs/tags/v\(.*\)|\1|g" | \
     sort -V -r
@@ -28,7 +28,7 @@ while read -r tag; do
   OUT="$SCRIPT_DIR/../versions/$tag.json"
   [ -f "$OUT" ] && continue # skip version if file exists
   URL="https://github.com/gohugoio/hugo/releases/download/v$tag/hugo_$tag""_checksums.txt"
-  python "$SCRIPT_DIR/extract_versions.py" "$tag" <(curl -L "$URL") "$OUT" || true
+  python "$SCRIPT_DIR/extract_versions.py" "$tag" <(curl -L --retry 5 "$URL") "$OUT" || true
 done < "$TMP"
 
 head -n 1 "$TMP" > "$SCRIPT_DIR/../versions/latest"

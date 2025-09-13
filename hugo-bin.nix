fca29f7f65c05f839458a81329a1f6d85bba35e9
Shogo Takata <peshogo@gmail.com>
2025-09-14 03:14:56 +0900

    hugo-bin: add manual and completions

--- hugo-bin.nix
+++ hugo-bin.nix
@@ -1,10 +1,12 @@
 {
-  stdenvNoCC,
+  stdenv,
   lib,
   libgcc,
   autoPatchelfHook,
   fetchurl,
   versionCheckHook,
+  buildPackages,
+  installShellFiles,
 
   # custom
   version,
@@ -13,31 +15,61 @@
 }:
 let
   inherit (lib) optionals;
-  platform-info = per-system-info.${stdenvNoCC.hostPlatform.system} or (throw "unsupported system");
+  platform-info = per-system-info.${stdenv.hostPlatform.system} or (throw "unsupported system");
   # non default variants on linux need to link with shared library
-  needsPatching = kind != "default" && stdenvNoCC.hostPlatform.isLinux;
+  needsPatching = kind != "default" && stdenv.hostPlatform.isLinux;
+
+  hugo-bin-unwrapped = stdenv.mkDerivation {
+    pname = "hugo-${kind}-bin-unwrapped";
+    inherit version;
+
+    src = fetchurl { inherit (platform-info) url hash; };
+    sourceRoot = ".";
+
+    nativeBuildInputs = optionals needsPatching [ autoPatchelfHook ];
+    buildInputs = optionals needsPatching [ libgcc.lib ];
+
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+      install -Dt $out/bin hugo
+      runHook postInstall
+    '';
+
+    nativeCheckInputs = [ versionCheckHook ];
+    doInstallCheck = true;
+    versionCheckProgramArg = "version";
+  };
 in
-stdenvNoCC.mkDerivation {
+stdenv.mkDerivation {
   pname = "hugo-${kind}-bin";
   inherit version;
 
-  src = fetchurl { inherit (platform-info) url hash; };
-  sourceRoot = ".";
+  nativeBuildInputs = [ installShellFiles ];
 
-  nativeBuildInputs = optionals needsPatching [ autoPatchelfHook ];
-  buildInputs = optionals needsPatching [ libgcc.lib ];
-
+  dontUnpack = true;
   dontBuild = true;
 
-  installPhase = ''
-    runHook preInstall
-    install -Dt $out/bin hugo
-    runHook postInstall
-  '';
+  installPhase =
+    let
+      emulator = stdenv.hostPlatform.emulator buildPackages;
+    in
+    ''
+      runHook preInstall
 
-  nativeCheckInputs = [ versionCheckHook ];
-  doInstallCheck = true;
-  versionCheckProgramArg = "version";
+      mkdir -p "$out/bin"
+      ln -s "${hugo-bin-unwrapped}/bin/hugo" "$out/bin/hugo"
+
+      ${emulator} "$out/bin/hugo" gen man
+      installManPage man/*
+      installShellCompletion --cmd hugo \
+        --bash <(${emulator} "$out/bin/hugo" completion bash) \
+        --fish <(${emulator} "$out/bin/hugo" completion fish) \
+        --zsh  <(${emulator} "$out/bin/hugo" completion zsh)
+
+      runHook postInstall
+    '';
 
   meta = {
     changelog = "https://github.com/gohugoio/hugo/releases/tag/v${version}";
